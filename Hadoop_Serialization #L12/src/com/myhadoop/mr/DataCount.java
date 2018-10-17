66a17eb0baeb2d9e04e7e314d94a56153ab60401
“ <841160480@qq.com>
2018-10-17 17:27:04 +0800

    Partitioner. #L14

--- Hadoop_Serialization #L12/src/com/myhadoop/mr/DataCount.java
+++ Hadoop_Serialization #L12/src/com/myhadoop/mr/DataCount.java
@@ -5,6 +5,8 @@
 package com.myhadoop.mr;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -13,6 +15,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -42,14 +45,21 @@ public class DataCount {
         job.setMapOutputValueClass(DataBean.class);
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         
+        //告诉Job我们自定义了分区功能
+        job.setPartitionerClass(ProviderPartitioner.class);
         
         
         job.setReducerClass(DCReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DataBean.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        //FileInputFormat.setInputPaths(job, new Path(args[0]));
-    
+        
+        //设置Reducer的数量，默认情况下只启动一个Reducer，一个Reducer对应一个文件，
+        //我们现在想要得到4个文件，自然而然我们得启动多个Reducer，
+        //为了程序的灵活性我们通过参数的形式给它赋值。
+        job.setNumReduceTasks(Integer.parseInt(args[2]));
+        
+        
         job.waitForCompletion(true);
     }
     
@@ -75,27 +85,53 @@ public class DataCount {
     public static class DCReducer extends Reducer<Text,DataBean,Text,DataBean>{
 
 		@Override
-		protected void reduce(Text arg0, Iterable<DataBean> arg1, Reducer<Text, DataBean, Text, DataBean>.Context arg2)
+		protected void reduce(Text key, Iterable<DataBean> v2s, Reducer<Text, DataBean, Text, DataBean>.Context context)
 				throws IOException, InterruptedException {
 			// TODO Auto-generated method stub
-			super.reduce(arg0, arg1, arg2);
+			long up_sum=0;
+			long down_sum=0;
+			for(DataBean bean:v2s) {
+				up_sum+=bean.getUpPayLoad();
+				down_sum+=bean.getDownPayLoad();
+			}
+			DataBean bean = new DataBean(key.toString(),up_sum,down_sum);
+			context.write(key, bean);
 		}
+    	
+    }
+    
+    public static class ProviderPartitioner extends Partitioner<Text,DataBean>{
+    	 /**
+         * numPartitions---分区的值是由Reducer的数量决定的，起几个Reducer就创建几个分区
+         */
+    	public static Map<String,Integer> providerMap= new HashMap<String,Integer>();
 
+    	static {
+    		providerMap.put("135", 1);
+    		providerMap.put("136", 1);
+    		providerMap.put("137", 1);
+    		providerMap.put("138", 1);
+    		providerMap.put("139", 1);
+    		providerMap.put("150", 2);
+    		providerMap.put("159", 2);
+    		providerMap.put("182", 3);
+    		providerMap.put("183", 3);
+    	}
     	
-//		@Override
-//		protected void reduce(Text key, Iterable<DataBean> v2s, Reducer<Text, DataBean, Text, DataBean>.Context context)
-//				throws IOException, InterruptedException {
-//			// TODO Auto-generated method stub
-//			long up_sum=0;
-//			long down_sum=0;
-//			for(DataBean bean:v2s) {
-//				up_sum+=bean.getUpPayLoad();
-//				down_sum+=bean.getDownPayLoad();
-//			}
-//			DataBean bean = new DataBean(key.toString(),up_sum,down_sum);
-//			context.write(key, bean);
-//		}
-    	
+		@Override
+		public int getPartition(Text key, DataBean dataBean, int numPartitions) {
+			// TODO Auto-generated method stub
+			//key是电话号码
+			String telNo = key.toString();
+			//我们截取前3位
+			//通过前三位就可以知道是移动、联通还是电信或是其它
+			String sub_tel = telNo.substring(0, 3);
+			Integer num = providerMap.get(sub_tel);
+			if(num==null) {
+				return 0;
+			}
+			return num;
+		}
     }
     
 }
